46764fbfc971bd5986e235c66dfc49c8dd3c7c79
Tyler Burch <burcht11@gmail.com>
2023-05-02 20:15:04 -0400

    Add selectbox, google analytics

--- web_app.py
+++ web_app.py
@@ -7,6 +7,11 @@ from datetime import datetime, date
 from pathlib import Path
 import glob
 import re
+import pathlib
+from bs4 import BeautifulSoup
+import logging
+import shutil
+
 # Data formatting 
 # -------------------------------------------------------------------------------------
 team_colors = {
@@ -57,21 +62,18 @@ dates = [
 ]
 latest_date = max(dates)
 latest_date_str = latest_date.strftime("%Y-%m-%d")
-output_df = pd.read_parquet(directory_path / f"data/prepped/{latest_date_str}.pq")
 
-# Web App 
-# -------------------------------------------------------------------------------------
-st.set_page_config(
-    page_title="NHL Playoff Predictions", 
-    layout="wide",
-    )
-st.title("NHL Playoff Predictions")
 
-st.write("""
-Below are predictions for the remainder of the 2023 NHL playoffs. The underlying model assumes that the rate at which each team scores and gives up points are realizations of latent "goal scoring" and "goal suppressing" parameters. Additionally, a factor for home-ice advantage is incorporated. The model is fit to the 2022-2023 regular season data.
-""")
-         
-st.write(f"Last updated: {latest_date_str}")
+def load_data_for_date(date_str):
+    output_df = pd.read_parquet(directory_path / f"data/prepped/{date_str}.pq")
+    file_path = directory_path / Path(f"data/prepped/matchups_{date_str}.pq")
+    if file_path.exists():
+        matchup_prob_df = pd.read_parquet(file_path)
+        matchup_prob_df['Home Win Probability'] = matchup_prob_df['Home Win Probability'].astype(int).astype(str) + '%'
+        matchup_prob_df['Away Win Probability'] = matchup_prob_df['Away Win Probability'].astype(int).astype(str) + '%'
+    else:
+        matchup_prob_df = None
+    return output_df, matchup_prob_df
 
 # Function to apply the color gradient to table cells
 def highlight_cells(val):
@@ -86,6 +88,56 @@ def highlight_cells(val):
     return f"background-color: {color}; color: {text_color};"
 
 
+def inject_ga():
+    GA_ID = "google_analytics"
+
+
+    GA_JS = """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WQ3JL3NLY9"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-WQ3JL3NLY9');
+    </script>
+    """
+
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID): 
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  
+        else:
+            shutil.copy(index_path, bck_index)  
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+
+
+# Web App 
+# -------------------------------------------------------------------------------------
+st.set_page_config(
+    page_title="NHL Playoff Predictions", 
+    layout="wide",
+    )
+st.title("NHL Playoff Predictions")
+st.write(f"Last updated: {latest_date_str}")
+st.write("""
+Below are predictions for the remainder of the 2023 NHL playoffs. The underlying model assumes that the rate at which each team scores and gives up points are realizations of latent "goal scoring" and "goal suppressing" parameters. Additionally, a factor for home-ice advantage is incorporated. The model is fit to the 2022-2023 regular season data.
+""")
+
+# Add a dropdown box for selecting the date
+selected_date = st.selectbox("Load projections from date:", options=[d.strftime("%Y-%m-%d") for d in sorted(dates, reverse=True)], index=0)
+
+# Load the data for the selected date
+output_df, matchup_prob_df = load_data_for_date(selected_date)
+
 # Display the table with formatting and sorting enabled
 st.dataframe(
     output_df.style.format("{:.0f}%").applymap(highlight_cells).set_table_styles(
@@ -93,12 +145,8 @@ st.dataframe(
     ]),
 )
 
-# Add daily matchup probailabilities
-file_path = directory_path / Path(f"data/prepped/matchups_{latest_date_str}.pq")
-if file_path.exists():
-    matchup_prob_df = pd.read_parquet(file_path)
-    matchup_prob_df['Home Win Probability'] = matchup_prob_df['Home Win Probability'].astype(int).astype(str) + '%'
-    matchup_prob_df['Away Win Probability'] = matchup_prob_df['Away Win Probability'].astype(int).astype(str) + '%'
+# Add daily matchup probabilities
+if matchup_prob_df is not None:
     # CSS to inject contained in a string
     hide_table_row_index = """
                 <style>
@@ -111,9 +159,13 @@ if file_path.exists():
     st.markdown(hide_table_row_index, unsafe_allow_html=True)
     st.table(matchup_prob_df)
 
+
+st.markdown("-----")
 st.write("""
 Model and visuals all produced by Tyler Burch. For more information on the methodology, see [this blog post](https://tylerjamesburch.com/blog/misc/nhl-predictions)\
 """)
 st.caption("""
 Thanks for visiting - if you enjoy this content, please [help me keep the app alive!](https://bmc.link/tylerjamesburch)
            """)
+
+inject_ga()
